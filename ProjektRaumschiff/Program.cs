e13282f905d7b8353c072421feb2df80bba0218b
melissa.wilkins <melissa.wilkins@espas.ch>
2020-06-25 09:47:33 +0200

    projekt structure

---
+++ ProjektRaumschiff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjektRaumschiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
