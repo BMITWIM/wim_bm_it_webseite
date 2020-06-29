02113e7b3b65bd5daaa0aba9c1deeecab0ffe8b2
Melissa Wilkins <melissa.wilkins@espas.ch>
2020-06-29 21:24:59 +0200

    commit 1 zu hause

--- ProjektRaumschiffTest/obj/Release/netcoreapp3.1/ProjektRaumschiffTest.AssemblyInfo.cs
+++ ProjektRaumschiffTest/obj/Release/netcoreapp3.1/ProjektRaumschiffTest.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("NUnitTestProject1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ProjektRaumschiffTest")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("NUnitTestProject1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("NUnitTestProject1")]
+[assembly: System.Reflection.AssemblyProductAttribute("ProjektRaumschiffTest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ProjektRaumschiffTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
