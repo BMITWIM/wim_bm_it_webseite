910fc5ef63a474c804b6589d77aa5c60b5908141
melissa.wilkins <melissa.wilkins@espas.ch>
2020-06-29 10:22:17 +0200

    test case fehler

--- ProjektRaumschiffTest/RaumschiffVorlageTest.cs
+++ ProjektRaumschiffTest/RaumschiffVorlageTest.cs
@@ -3,18 +3,25 @@ using System;
 using System.IO;
 using ProjektRaumschiff;
 
-
+namespace ProjektRaumschiff
 {
+
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
-         Raumschiffvorlage("test", 10)
+            Raumschiffvorlage test = new Raumschiffvorlage();
+            Raumschiffvorlage alba = new Raumschiffvorlage("alba", 10);
+
+            Assert.AreEqual(test.getSchiffName(), "Enterprise");
+            Assert.AreEqual(test.getSchiffgeschwindigkeit(), 100);
+            Assert.AreEqual(alba.getSchiffName(), "alba");
+            Assert.AreEqual(alba.getSchiffgeschwindigkeit(), 10);
         }
 
         [Test]
-        public void Test1()
+        public void ZeigeStautsTest()
         {
             Assert.Pass();
         }
