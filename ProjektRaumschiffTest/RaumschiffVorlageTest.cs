a2b4137823caf42d1863daf29a02f224c5c81434
melissa.wilkins <melissa.wilkins@espas.ch>
2020-07-01 11:17:25 +0200

    my first commit

--- ProjektRaumschiffTest/RaumschiffVorlageTest.cs
+++ ProjektRaumschiffTest/RaumschiffVorlageTest.cs
@@ -8,24 +8,38 @@ namespace ProjektRaumschiff
 
     public class Tests
     {
+        Raumschiffvorlage test = new Raumschiffvorlage();
+        Raumschiffvorlage alba = new Raumschiffvorlage("alba", 10);
+        Raumschiffvorlage duga = new Raumschiffvorlage("duga", 40);
+            
         [SetUp]
         public void Setup()
         {
-            Raumschiffvorlage test = new Raumschiffvorlage();
-            String sn = "alba";
-            int sg = 10;
-            Raumschiffvorlage alba = new Raumschiffvorlage("alba", 10);
+            
 
             Assert.AreEqual(test.getSchiffName(), "Enterprise");
             Assert.AreEqual(test.getSchiffgeschwindigkeit(), 100);
             Assert.AreEqual("alba", alba.getSchiffName() );
             Assert.AreEqual(10, alba.getSchiffgeschwindigkeit());
+            Assert.AreEqual("duga", duga.getSchiffName());
+            Assert.AreEqual(40, duga.getSchiffgeschwindigkeit());
+            Assert.AreEqual(test.getLeben(), 50);
+            Assert.AreEqual(alba.getLeben(), 50);
         }
 
         [Test]
-        public void ZeigeStautsTest()
+        public void KollisionTest()
         {
-            
+            alba.Kollision();
+
+            Assert.Less(alba.getLeben(), 50); ;
+        }
+
+        [Test]
+        public void Game()
+        {
+            Game(alba, duga);
+
         }
     }
 }
