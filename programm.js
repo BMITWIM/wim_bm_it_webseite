dc2a4dce382506a341de9226cca23e97c43e7273
melissa.wilkins <melissa.wilkins@espas.ch>
2020-09-25 17:03:19 +0200

    displayGreeting

--- programm.js
+++ programm.js
@@ -3,6 +3,14 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "Hello Javascript!";
 }
 
-function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
+function displayGreeting() {
+    var d = new Date();
+    if (d.getHours < 12){
+        document.getElementById("demo").innerHTML = "Good morning!"
+    }
+    if(d.getHours < 19){
+        document.getElementById("demo").innerHTML = "Good afternoon!"
+    } else{
+    document.getElementById("demo").innerHTML = "Good evening!";
+    }
 }
