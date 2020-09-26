053d93f99ee81e92785f1f64d998f301e8c62ddc
Melissa Wilkins <melissa.wilkins@espas.ch>
2020-09-26 08:18:53 +0200

    displayGreting() works!

--- programm.js
+++ programm.js
@@ -1,16 +1,17 @@
 
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello Javascript!";
+    var y = new Date();
+    document.getElementById("demo").innerHTML = y.getHours();
 }
 
 function displayGreeting() {
     var d = new Date();
-    if (d.getHours < 12){
+    if (d.getHours() < 12 && d.getHours() >= 0){
         document.getElementById("demo").innerHTML = "Good morning!"
     }
-    if(d.getHours < 19){
+    if(d.getHours() < 19 && d.getHours() >= 12){
         document.getElementById("demo").innerHTML = "Good afternoon!"
-    } else{
+    } if (d.getHours() <= 23 && d.getHours() >= 19) {
     document.getElementById("demo").innerHTML = "Good evening!";
     }
 }
